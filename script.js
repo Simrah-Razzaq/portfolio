37c787d9490bc3698d42f136ff4c3286bc875b66
Simrah-Razzaq <simrahkumbhar@gmail.com>
2025-01-15 19:54:38 +0500

    Portfolio

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function skills() {
+    document.getElementById("details").innerHTML = `
+        <h1>Skills</h1>
+    <ul>
+        <li>Proficient in MS Office (Word, Excel, PowerPoint, etc.).</li>
+        <li>Knowledge of HTML, CSS, and Basic JavaScript for web development.</li>
+        <li>Familiar with basic programming concepts and DOM manipulation.</li>
+        <li>Strong written and verbal communication skills.</li>
+        <li>Ability to create engaging presentations and educational materials.</li>
+    </ul>
+    <h1>Intrest</h1>
+    <ul>
+        <li>Passion for teaching and sharing knowledge, especially in computer-related subjects.</li>
+        <li>Exploring and learning new technologies to stay updated in the tech field.</li>
+        <li>Enthusiastic about web design and building interactive user interfaces.</li>
+        <li>Interested in pursuing advanced graphic design and animation courses.</li>
+        <li>Keen on developing creative and practical solutions for real-world challenges through technology.</li>
+    </ul>
+    
+    `;
+}
+function about() {
+    document.getElementById("details").innerHTML = ` <h1>About Me</h1>
+                <p>
+
+                    Hello! <br>
+                    My name is Simrah Abdul Razzaq. I completed my Matriculation in 2023 from Al-Habib Iqra Academy
+                    School and will, InshaAllah, complete my Intermediate in 2025 from For Women College. I have a deep
+                    interest in computers and technology, and my ultimate goal is to become a Software Engineer.
+
+                    I have also completed a computer course and am currently studying Introduction to Programming at
+                    SMIT. Additionally, I am proficient in MS Office and plan to pursue courses in Graphic Design and
+                    Animation in the future.
+
+                    I am to use my education and skills to build a successful career and make a significant impact in
+                    the field of technology. </p>`
+}
+
+function project() {
+    document.getElementById("details").innerHTML = `
+    <h1>My Projects</h1>
+    <img src="assests/project-1.PNG" alt="">
+    <img src="assests/project2.PNG" alt="">
+    <img src="assests/project3.PNG" alt="">
+    <img src="assests/project4.PNG" alt="">
+    <img src="assests/project5.PNG" alt="">`
+    
+}
